d9f9737f0ca1e3ac7651605d05d64c2301fa5723
Rena Auerbach <renaauerbach@gmail.com>
2020-04-28 13:59:35 -0500

    Updated Nav and added media files

---
+++ scripts.js
@@ -0,0 +1,18 @@
+/***************** NAVIGATION PAGE *******************/
+
+function hover() {
+    let obj = document.getElementById('#palette');
+}
+
+/***************** NETFLIX PAGE *******************/
+
+function flyIn() {
+    let grid = document.querySelector('.fly-in');
+    setTimeout(() => {
+        grid.removeClass('hidden');
+    }, 500);
+}
+
+/******************* DOG PAGE *********************/
+
+/******************* COLOR PAGE *******************/
