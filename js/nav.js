612b274c1ab4754162a04d0240dcb35b3aa1e0a8
Rena Auerbach <renaauerbach@gmail.com>
2020-05-04 16:37:44 -0500

    Nav circle generator

--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,46 @@
 /***************** NAVIGATION PAGE *******************/
 
 let startTime = new Date().getTime();
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+let svgNS = 'http://www.w3.org/2000/svg';
+let svg = document.getElementById('circles');
+svg.setAttribute('viewBox', '0 0 ' + width + ' ' + height);
+
 let drawCircles = setInterval(() => {
-    if (new Date().getTime() - startTime > 30000) {
-        clearInterval(drawCircles);
-        return;
-    }
     let colors = [
-        'rgba(255, 255, 255, 0.2)',
-        'rgba(255, 255, 255, 0.4)',
-        'rgba(255, 255, 255, 0.6)',
-        'rgba(255, 255, 255, 0.8)',
-        'rgba(255, 255, 255, 1)',
+        '#ed4b42',
+        '#f775c3',
+        '#bd2abd',
+        '#9517ad',
+        '#8b65ce',
+        '#5a65e0',
+        '#8ec2fa',
+        '#107bf0',
+        '#45bfdd',
+        '#3bc9c9',
+        '#26b44e',
+        '#167e1a',
+        '#72ce3e',
+        '#ffff00',
+        '#ffd346',
+        '#ff9900',
+        '#ff7000',
     ];
 
-    let container = document.querySelector('body');
-    let circle;
+    // let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    // let svgNS = svg.namespaceURI;
+    let circle = document.createElementNS(svgNS, 'circle');
+    // Math.floor(Math.random() * (max - min + 1) + min)
 
-    circle.style.color = colors[Math.floor(Math.random() * colors.length)];
-    circle.style.top = Math.floor(Math.random() * fullHeight + 1) + 'px';
+    circle.setAttribute(
+        'fill',
+        colors[Math.floor(Math.random() * colors.length)]
+    );
+    circle.setAttribute('r', Math.floor(Math.random() * (100 - 20 + 1) + 20));
+    circle.setAttribute('cx', Math.floor(Math.random() * width) + 1);
+    circle.setAttribute('cy', Math.floor(Math.random() * height) + 1);
 
-    container.appendChild(circle);
-}, 2000);
-
-window.addEventListener('load', drawCircles);
+    svg.appendChild(circle);
+}, 1000);
